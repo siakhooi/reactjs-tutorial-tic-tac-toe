827b50d9d45f8c6aa1d14624710c9a60ce86f31d
Siak Hooi Ng <siakhooi@gmail.com>
2021-08-21 12:55:14 +0800

    add screen test

---
+++ src/__test__/Game.screen.test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import Game from '../Game'
+
+it('Game/screen', () => {
+  render(<Game />)
+  const linkElement1 = screen.getByText(/Next player:/i)
+  expect(linkElement1).toBeInTheDocument()
+
+  const linkElement2 = screen.getByText(/Go to game start/i)
+  expect(linkElement2).toBeInTheDocument()
+})
