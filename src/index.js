efc039d057ca3c708c749c604b1a5ab4723de822
Siak Hooi <73884114+siakhooi@users.noreply.github.com>
2024-03-03 05:13:18 +0000

    fix bug - container not defined

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import './index.css'
 import Game from './Game'
 
 import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<Game />, document.getElementById('root'));
+root.render(<Game />);
